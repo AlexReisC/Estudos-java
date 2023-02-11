4e2b442b00f24b33d95e4f39d70d3a327e6065bf
AlexReisC <alex.reis@aluno.ufca.edu.br>
2023-02-10 22:58:40 -0300

    exercicio 02, positivos e media

---
+++ Exercicios/src/basicos/positivosMedia.java
@@ -0,0 +1,31 @@
+package basicos;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class positivosMedia {
+    public static void main(String[] args) throws IOException {
+
+		try (Scanner leitor = new Scanner(System.in)) {
+            int cont = 0;
+            double media = 0;
+            double x;
+            
+            int i;
+            
+            for (i = 0; i < 6; i++) {
+            	x = leitor.nextDouble();
+            	if(x > 0){
+            		cont++;
+            		media = media + x;
+            	}
+            }
+            leitor.close();
+            
+            media = media/cont;
+            System.out.println(cont + " valores positivos");
+            System.out.println(String.format("%.1f", media));
+        }
+
+    }
+}
