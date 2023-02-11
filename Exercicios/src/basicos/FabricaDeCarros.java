a79c478299190fc3d611abc04391dc3e094e73fa
AlexReisC <alex.reis@aluno.ufca.edu.br>
2023-02-10 22:52:05 -0300

    exercicio 01

--- Exercicios/src/basicos/FabricaDeCarros.java
+++ Exercicios/src/basicos/FabricaDeCarros.java
@@ -1,7 +1,25 @@
 package basicos;
 
+import java.util.*;
+
 public class FabricaDeCarros {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+	    int custoFabrica = scan.nextInt();
+	    int porcentagemDistribuidor = scan.nextInt();
+	    int PercentualImpostos = scan.nextInt();
+        
+        int custoConsumidor;
+        int Distribuidor;
+        int ValorImpostos;
+ 
+        Distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+        ValorImpostos = (custoFabrica * PercentualImpostos) / 100;
+        custoConsumidor = custoFabrica + Distribuidor + ValorImpostos;
+        
+        scan.close();
+        System.out.println(custoConsumidor);
         
     }
 }
