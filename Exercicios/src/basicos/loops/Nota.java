8c545d0aaa8f332da7b1bc5ffd28aec591aff1c8
AlexReisC <alex.reis@aluno.ufca.edu.br>
2023-02-18 21:25:39 -0300

    exercicio pedir nota valida

--- Exercicios/src/basicos/loops/Nota.java
+++ Exercicios/src/basicos/loops/Nota.java
@@ -1,7 +1,20 @@
 package basicos.loops;
 
+import java.util.Scanner;
+
+/* Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. */
+
 public class Nota {
     public static void main(String[] args) {
-        
+        double nota;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("Digite um valor entre 0 e 10: ");
+            nota = scan.nextDouble();
+            if(nota < 0 || nota > 10) System.out.println("Valor inválido!");
+        } while (nota < 0 || nota > 10);
+
+        scan.close();
     }
 }
